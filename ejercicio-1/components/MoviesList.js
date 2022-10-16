14b52a57dfab1e5a55431797939751c45297b0b6
Cesron <cesronvasquez@gmail.com>
2022-10-16 14:05:22 -0600

    Initial commit

---
+++ ejercicio-1/components/MoviesList.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export function MoviesList() {
+  return (
+    <View>
+      <Text>MoviesList</Text>
+    </View>
+  );
+}
